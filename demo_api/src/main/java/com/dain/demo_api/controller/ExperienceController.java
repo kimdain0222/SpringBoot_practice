bc2d4703848a2d0a20f8bd3bf8133e1fea3e475d
neolo <neolove0125@gmail.com>
2026-02-03 16:05:24 +0900

    서비스와 컨트롤러 이해- 얇게 만들기

--- demo_api/src/main/java/com/dain/demo_api/controller/ExperienceController.java
+++ demo_api/src/main/java/com/dain/demo_api/controller/ExperienceController.java
@@ -1,18 +1,49 @@
-package com.dain.demo_api.controller;
+// Controller = 외부 요청(HTTP)을 처음 받는 ‘입구’ -ex)정문에서 “어디 부서로 보낼지” 정해주는 직원
+// 요청 받기 (URL, HTTP Method), 요청에 맞는 로직 호출, 응답 반환
+
+
+package com.dain.demo_api.controller; //controller 역할을 담당
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+//이 클래스는 HTTP 요청을 처리하는 컨트롤러고, GET 요청을 받을 거야
 
 import com.dain.demo_api.dto.HelloResponse;
+import com.dain.demo_api.service.ExperienceService;
+//HelloResponse DTO를 가져온다는 뜻.
+//데이터를 직접 정의x, DTO 클래스를 통해 구조화된 데이터를 반환하는 것
 
-@RestController
+@RestController // REST API 전용 컨트롤러
 public class ExperienceController {
+//JSON 데이터로 바로 응답을 반환하는 컨트롤러 클래스 = 이 컨트롤러는 REST API 전용이라는 뜻
+    private final ExperienceService experienceService;
 
-    @GetMapping("/hello")
-    public HelloResponse hello() {
-        return new HelloResponse(
-                "Spring Boot JSON 응답 성공!",
-                "dain"
-        );
+    public ExperienceController(ExperienceService experienceService) {
+        this.experienceService = experienceService;
     }
+
+    // "/hello" 경로로 들어오는 GET 요청 처리
+    @GetMapping("/hello")
+    public HelloResponse hello(@RequestParam String name) {
+        // 실제 데이터 생성 로직은 Service 계층에 위임
+        return experienceService.getHelloMessage(name);
+        
+    }
+    
 }
+
+
+// <구조>
+//브라우저
+//  ↓ GET /hello
+//ExperienceController
+//  ↓ HelloResponse 객체 생성
+//Spring (Jackson)
+//  ↓ JSON 변환
+//브라우저
+
+// <설명>
+// Controller → 입구
+// DTO → 데이터 모양
+// Service → 로직
---
+++ demo_api/src/main/java/com/dain/demo_api/service/ExperienceService.java
@@ -0,0 +1,20 @@
+package com.dain.demo_api.service;
+
+import org.springframework.stereotype.Service;
+import com.dain.demo_api.dto.HelloResponse;
+
+@Service
+public class ExperienceService {
+
+    public HelloResponse getHelloMessage(String name) {
+        return new HelloResponse(
+                "안녕하세요 " + name + "!",
+            "service"
+        );
+    }
+}
+
+
+// controller → 요청/응답
+// service → 로직
+// dto → 데이터 모양
