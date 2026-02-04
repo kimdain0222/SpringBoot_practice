3c660b8ac4a834d91e918614143bee7d85ddc375
neolo <neolove0125@gmail.com>
2026-02-04 11:10:02 +0900

    feat: add Request DTO and validation 실전 경험 타파~!

--- demo_api/src/main/java/com/dain/demo_api/controller/ExperienceController.java
+++ demo_api/src/main/java/com/dain/demo_api/controller/ExperienceController.java
@@ -1,42 +1,40 @@
-// Controller = 외부 요청(HTTP)을 처음 받는 ‘입구’ -ex)정문에서 “어디 부서로 보낼지” 정해주는 직원
+// Controller = 외부 요청(HTTP)을 처음 받는 ‘입구’
+// ex) 정문에서 “어디 부서로 보낼지” 정해주는 직원
 // 요청 받기 (URL, HTTP Method), 요청에 맞는 로직 호출, 응답 반환
 
+package com.dain.demo_api.controller; // controller 역할을 담당
 
-package com.dain.demo_api.controller; //controller 역할을 담당
-
+// Spring Web 관련 import
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//이 클래스는 HTTP 요청을 처리하는 컨트롤러고, GET 요청을 받을 거야
+import jakarta.validation.Valid;
 
+// DTO import
+import com.dain.demo_api.dto.HelloRequest;
 import com.dain.demo_api.dto.HelloResponse;
+
+// Service import
 import com.dain.demo_api.service.ExperienceService;
-//HelloResponse DTO를 가져온다는 뜻.
-//데이터를 직접 정의x, DTO 클래스를 통해 구조화된 데이터를 반환하는 것
 
-
+// 이 클래스는 HTTP 요청을 처리하는 컨트롤러고, GET 요청을 받을 거야
 @RestController // REST API 전용 컨트롤러
 public class ExperienceController {
-//JSON 데이터로 바로 응답을 반환하는 컨트롤러 클래스 = 이 컨트롤러는 REST API 전용이라는 뜻
+
+    // JSON 데이터로 바로 응답을 반환하는 컨트롤러 클래스
+    // = 이 컨트롤러는 REST API 전용이라는 뜻
     private final ExperienceService experienceService;
 
+    // 생성자 주입 (Spring이 Service 객체를 주입해줌)
     public ExperienceController(ExperienceService experienceService) {
         this.experienceService = experienceService;
     }
 
     // "/hello" 경로로 들어오는 GET 요청 처리
     @GetMapping("/hello")
-    public HelloResponse hello(@RequestParam(required = false) String name) //name이 없어도 요청은 들어오게 허용
-     {
+    public HelloResponse hello(@Valid HelloRequest request) {
         // 실제 데이터 생성 로직은 Service 계층에 위임
-            if (name == null || name.isBlank()) { //우리가 직접 입력 검증하겠다
-        throw new IllegalArgumentException("name 파라미터는 필수입니다");
-    }    //“이 요청은 잘못됐다”라고 의도적으로 예외 발생시키는 것
-
-        return experienceService.getHelloMessage(name);
-        
+        return experienceService.getHelloMessage(request.getName());
     }
-    
 }
 
 
---
+++ demo_api/src/main/java/com/dain/demo_api/dto/HelloRequest.java
@@ -0,0 +1,22 @@
+package com.dain.demo_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class HelloRequest {
+
+    @NotBlank(message = "name은 필수입니다")  //if문을 대체하는 선언적 검증
+    private String name;
+
+    // 기본 생성자 (Spring이 객체 만들 때 필요)객체를 먼저 만들고 → 값 채우기 위해
+    public HelloRequest() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) { //Spring은 요청 데이터를 setter를 통해 객체에 주입함
+        this.name = name;
+    }
+}
+
+
